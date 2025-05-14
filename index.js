25f3ba8b925e22b70f680dac4fb25745ef471549
Duvansg06 <140823687+Duvansg06@users.noreply.github.com>
2025-05-14 06:36:07 -0500

    Ejercicio1

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 
+// EJERCICIO 1
+// Crear una Promesa Simple
+// Escribir una promesa que resuelva después de 2 segundos con el mensaje
+// "Promesa cumplida". Luego, consumirla usando .then() para imprimir el
+// mensaje en consola.
+
+const prueba = (milisegundos) =>{
+    return new Promise ((resolve)=> {
+        setTimeout(() => {
+            resolve (milisegundos)
+            console.log("Promesa cumplida")
+            ;
+        })
+    } );
+};
+    
+prueba (2000).then((mensaje) => {
+    console.log(mensaje);
+}).catch((error) => {
+    console.error(error);
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
