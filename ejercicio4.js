e086278b75fe42bced46323c1e799ffdbfe47a1d
Diego SENA <duvierdiegodavid.0715@gmail.com>
2025-05-14 07:37:12 -0500

    Ejercicio4 resuelto

--- ejercicio4.js
+++ ejercicio4.js
@@ -3,3 +3,28 @@
 Crear tres promesas que se resuelvan en diferentes tiempos (por ejemplo, 1s, 2s, 3s).
 Usar Promise.all() para esperar a que todas se resuelvan e imprimir los resultados en un arreglo.
 */
+
+const promesa1 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Promesa 1 resuelta");
+    }, 1000);
+});
+const promesa2 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Promesa 2 resuelta");
+    }, 2000);
+});
+const promesa3 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Promesa 3 resuelta");
+    }, 3000);
+});
+
+Promise.all([promesa1, promesa2, promesa3])
+    .then((resultados) => {
+        console.log("Todas las promesas se resolvieron:");
+        console.log(resultados);
+    })
+    .catch((error) => {
+        console.error("Error en alguna de las promesas:", error);
+    });
